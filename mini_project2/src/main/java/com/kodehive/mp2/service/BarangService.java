958ac9d48fb30dd026244db124e92f6df5ab9eb1
radi2 <radi2@DESKTOP-IGJC24N>
2021-01-12 14:27:12 +0700

    add java file for BE BarangService

---
+++ mini_project2/src/main/java/com/kodehive/mp2/service/BarangService.java
@@ -0,0 +1,44 @@
+package com.kodehive.mp2.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.kodehive.mp2.model.BarangModel;
+import com.kodehive.mp2.repo.BarangRepository;
+
+
+@Service
+@Transactional
+public class BarangService { 
+
+	// Depencies Injection mempermudah metode 
+	@Autowired
+	private BarangRepository barangRepository;
+	
+	public List<BarangModel> temukan() {
+		return barangRepository.findAll();
+	}
+	
+	public void barangSave(BarangModel barangModel) {
+		barangRepository.save(barangModel);
+	
+	}
+	
+	public BarangModel barangUpdate(BarangModel barangModel) {
+		 return barangRepository.save(barangModel);
+	}
+
+	public void barangHapus(String kdBarang) {
+		// TODO Auto-generated method stub
+		barangRepository.deleteById(kdBarang);
+	}
+	
+	public BarangModel barangIDBaca(String idBarang) {
+		return barangRepository.noBarang(idBarang);
+	}
+	
+
+} 
