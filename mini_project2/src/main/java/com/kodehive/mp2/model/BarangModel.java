122abaff07a7dd27320c3b77f1c5450db74dca5d
radi2 <radi2@DESKTOP-IGJC24N>
2021-01-12 13:38:55 +0700

    commit BarangModel

--- mini_project2/src/main/java/com/kodehive/mp2/model/BarangModel.java
+++ mini_project2/src/main/java/com/kodehive/mp2/model/BarangModel.java
@@ -10,7 +10,6 @@ import javax.persistence.Table;
 
 public class BarangModel {
 	
-	
 	@Id
 	@Column(name="T_KD_BARANG")
 	private String kdBarang;
@@ -27,72 +26,52 @@ public class BarangModel {
 	@Column(name="T_KETRANGAN")
 	private String keterangan;
 
-	/**
-	 * @return the kdBarang
-	 */
+	
 	public String getKdBarang() {
 		return kdBarang;
 	}
 
-	/**
-	 * @param kdBarang the kdBarang to set
-	 */
+	
 	public void setKdBarang(String kdBarang) {
 		this.kdBarang = kdBarang;
 	}
 
-	/**
-	 * @return the namaBarang
-	 */
+	
 	public String getNamaBarang() {
 		return namaBarang;
 	}
 
-	/**
-	 * @param namaBarang the namaBarang to set
-	 */
+	
 	public void setNamaBarang(String namaBarang) {
 		this.namaBarang = namaBarang;
 	}
 
-	/**
-	 * @return the satuan
-	 */
+	
 	public String getSatuan() {
 		return satuan;
 	}
 
-	/**
-	 * @param satuan the satuan to set
-	 */
+	
 	public void setSatuan(String satuan) {
 		this.satuan = satuan;
 	}
 
-	/**
-	 * @return the hargaSiswa
-	 */
+	
 	public long getHargaSiswa() {
 		return hargaSiswa;
 	}
 
-	/**
-	 * @param hargaSiswa the hargaSiswa to set
-	 */
+	
 	public void setHargaSiswa(long hargaSiswa) {
 		this.hargaSiswa = hargaSiswa;
 	}
 
-	/**
-	 * @return the keterangan
-	 */
+	
 	public String getKeterangan() {
 		return keterangan;
 	}
 
-	/**
-	 * @param keterangan the keterangan to set
-	 */
+	
 	public void setKeterangan(String keterangan) {
 		this.keterangan = keterangan;
 	}
