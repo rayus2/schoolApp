5439360a0082c5631eff7f679ffa72fefa9b06da
radi2 <radi2@DESKTOP-IGJC24N>
2021-01-17 22:59:29 +0700

    update BE Barang

--- mini_project2/src/main/java/com/kodehive/mp2/model/BarangModel.java
+++ mini_project2/src/main/java/com/kodehive/mp2/model/BarangModel.java
@@ -6,98 +6,70 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="T_BARANG")
+@Table(name="t_barang")
 
 public class BarangModel {
 	
 	
 	@Id
-	@Column(name="T_KD_BARANG")
+	@Column(name="kd_barang")
 	private String kdBarang;
 	
-	@Column(name="T_NAMA_BARANG")
+	@Column(name="nama_barang")
 	private String namaBarang;
 	
-	@Column(name="T_SATUAN")
+	@Column(name="satuan")
 	private String satuan;
 	
-	@Column(name="T_HARGA_SISWA")
+	@Column(name="harga_siswa")
 	private long hargaSiswa;
 	
-	@Column(name="T_KETRANGAN")
+	@Column(name="keterangan")
 	private String keterangan;
 
-	/**
-	 * @return the kdBarang
-	 */
 	public String getKdBarang() {
 		return kdBarang;
 	}
 
-	/**
-	 * @param kdBarang the kdBarang to set
-	 */
 	public void setKdBarang(String kdBarang) {
 		this.kdBarang = kdBarang;
 	}
 
-	/**
-	 * @return the namaBarang
-	 */
 	public String getNamaBarang() {
 		return namaBarang;
 	}
 
-	/**
-	 * @param namaBarang the namaBarang to set
-	 */
 	public void setNamaBarang(String namaBarang) {
 		this.namaBarang = namaBarang;
 	}
 
-	/**
-	 * @return the satuan
-	 */
 	public String getSatuan() {
 		return satuan;
 	}
 
-	/**
-	 * @param satuan the satuan to set
-	 */
 	public void setSatuan(String satuan) {
 		this.satuan = satuan;
 	}
 
-	/**
-	 * @return the hargaSiswa
-	 */
 	public long getHargaSiswa() {
 		return hargaSiswa;
 	}
 
-	/**
-	 * @param hargaSiswa the hargaSiswa to set
-	 */
 	public void setHargaSiswa(long hargaSiswa) {
 		this.hargaSiswa = hargaSiswa;
 	}
 
-	/**
-	 * @return the keterangan
-	 */
 	public String getKeterangan() {
 		return keterangan;
 	}
 
-	/**
-	 * @param keterangan the keterangan to set
-	 */
 	public void setKeterangan(String keterangan) {
 		this.keterangan = keterangan;
 	}
 	
 	
 	
+
+
 
 }
